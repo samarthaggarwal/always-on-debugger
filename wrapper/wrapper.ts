83f345948a5fc70f7c451d5d8e86b69aac4ed3f2
Amir <amirk_au@hotmail.com>
2024-09-08 11:03:30 -0700

    more

--- wrapper/wrapper.ts
+++ wrapper/wrapper.ts
@@ -1,5 +1,5 @@
 const { argv } = require('node:process');
-import { exec, execSync, spawn } from 'child_process';
+import { exec, execSync, spawn, spawnSync } from 'child_process';
 import { join } from 'path';
 
 const path = join('terminal');
@@ -7,13 +7,17 @@ console.log(path);
 
 async function run() {
   console.log(argv);
-  try {
-    execSync(`py -3.8 -m ${path}`);
-  } catch (error) {
-    console.log(error);
-  } finally {
-    console.log('done');
-  }
+
+  const child = execSync(`py -3.8 -m ${path}`, { stdio: 'inherit' });
+  // const child = execSync(`node somethinsg.js`, { stdio: 'inherit' });
+
+  // child.on('exit', () => {
+  //   console.log('exited now');
+  // });
+
+  // child.on('error', (error) => {
+  //   console.log('Error happened!', error);
+  // });
 }
 
 run();
