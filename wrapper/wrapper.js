1ae0c1598b39536d041ff41147869204d6036fea
Amir <amirk_au@hotmail.com>
2024-09-08 12:21:27 -0700

    more

--- wrapper/wrapper.js
+++ wrapper/wrapper.js
@@ -5,13 +5,21 @@ const { exec, execSync, spawn, spawnSync } = require('child_process');
 const { join } = require('path');
 
 const path = join('terminal');
-console.log(path);
+// console.log(path);
+
+// npm run start -- "py -3.8 -m terminal"
+// npm pack
+// npm i -g aod-0.1.tgz
+//
 
 async function run() {
-  console.log(argv);
+  // console.log(argv);
+  const [, , ...args] = argv;
+  const command = args.join(' ');
 
   try {
-    const child = execSync(`py -3.8 -m ${path}`, { stdio: 'inherit' });
+    // const child = execSync(`py -3.8 -m ${path}`, { stdio: 'inherit' });
+    const child = execSync(command, { stdio: 'inherit' });
   } catch (error) {
     console.log('Error happened', error);
   }
